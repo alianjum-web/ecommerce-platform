668d7e12ca907fcc73183b5d68891b8a663c01dd
alianjum7880-coder <alianjum7880@gmail.com>
2025-10-02 16:02:15 +0500

    Added validation in the route

--- server/src/routes/cartRoutes.ts
+++ server/src/routes/cartRoutes.ts
@@ -7,11 +7,12 @@ import {
   removeFromCart,
   updateCartItemQuantity,
 } from "../controllers/cartController";
-
+import { validate } from "../middleware/validation";
+import { addToCartSchema } from "../schema/cartSchema";
 const router = express.Router();
 
 router.get("/fetch-cart", authenticateJwt, getCart);
-router.post("/add-to-cart", authenticateJwt, addToCart);
+router.post("/add-to-cart", authenticateJwt, validate(addToCartSchema), addToCart);
 router.delete("/remove/:id", authenticateJwt, removeFromCart);
 router.put("/update/:id", authenticateJwt, updateCartItemQuantity);
 router.post("/clear-cart", authenticateJwt, clearEntireCart);
