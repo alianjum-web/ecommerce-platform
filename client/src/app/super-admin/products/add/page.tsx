a19dc305fffda64ead5ebe09f243e0e04515001c
alianjum-web <muhammadaliabbas7890@gmail.com>
2025-10-07 03:39:17 +0500

    stops build because not url at that time

--- client/src/app/super-admin/products/add/page.tsx
+++ client/src/app/super-admin/products/add/page.tsx
@@ -3,6 +3,8 @@
 import { Suspense } from "react";
 import ProductForm from "@/components/super-admin/ProductForm";
 
+export const dynamic = 'force-dynamic';
+
 function SuperAdminManageProductPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
