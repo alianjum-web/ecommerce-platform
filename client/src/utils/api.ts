0ae27493884dd8a22e92c6233ea760005779209b
alianjum-web <muhammadaliabbas7890@gmail.com>
2025-10-30 07:58:10 +0500

    update API_BASE_URL to use environment variable for production

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 // lib/api-config.ts
 export const API_BASE_URL = process.env.NODE_ENV === 'development' 
   ? 'http://localhost:4001/api'  // Development
-  : 'https://ecommerce-platform-1vre.onrender.com/api'; // Production
+  : process.env.NEXT_PUBLIC_API_URL; // Production
 
 export const API_ROUTES = {
   AUTH: `${API_BASE_URL}/auth`,
