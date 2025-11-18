ae9ab6a2c5dbdebd3269d5c7a68e6f3fd37c8e88
alianjum-web <muhammadaliabbas7890@gmail.com>
2025-11-18 07:24:25 +0500

    direct calll due to login failure with 405

--- client/src/store/useAuthStore.ts
+++ client/src/store/useAuthStore.ts
@@ -29,7 +29,8 @@ const getBaseURL = () => {
 };
 
 const axiosInstance = axios.create({
-  baseURL: getBaseURL(),
+  // baseURL: getBaseURL(),
+  baseURL: `${process.env.BACKEND_URL}/api/auth`,
   withCredentials: true,
   timeout: 10000,
 });
