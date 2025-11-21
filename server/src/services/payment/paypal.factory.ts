fca191983fb8de75b261a775b454f7a1f588a59f
alianjum-web <muhammadaliabbas7890@gmail.com>
2025-11-21 15:23:20 +0500

    Differnt dedicated payment methods added

---
+++ server/src/services/payment/paypal.factory.ts
@@ -0,0 +1,31 @@
+// services/payment/payment.factory.ts
+import { PaymentMethod } from '../../interfaces/payment.interface';
+import { PayPalService } from './paypal.service';
+import { StripeService } from './stripe.service';
+import { CardPaymentService } from './card-payment.service';
+
+export class PaymentFactory {
+  static createPaymentMethod(method: string): PaymentMethod {
+    switch (method.toUpperCase()) {
+      case 'PAYPAL':
+        return new PayPalService();
+      
+      case 'STRIPE':
+        return new StripeService();
+      
+      case 'CARD':
+      case 'CREDIT_CARD':
+      case 'DEBIT_CARD':
+      case 'VISA':
+      case 'MASTERCARD':
+        return new CardPaymentService();
+      
+      default:
+        throw new Error(`Payment method '${method}' is not supported`);
+    }
+  }
+
+  static getAvailableMethods(): string[] {
+    return ['PAYPAL', 'STRIPE', 'CARD', 'VISA', 'MASTERCARD'];
+  }
+}
