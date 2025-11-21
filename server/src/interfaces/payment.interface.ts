787f1a2cf91ed01274231bec98e0af68c3b07fd1
alianjum-web <muhammadaliabbas7890@gmail.com>
2025-11-21 15:20:15 +0500

    types for the payment added

---
+++ server/src/interfaces/payment.interface.ts
@@ -0,0 +1,29 @@
+// interfaces/payment.interface.ts
+export interface PaymentMethod {
+  createOrder(orderData: any): Promise<any>;
+  capturePayment(paymentId: string, data?: any): Promise<any>;
+  validatePayment(data: any): boolean;
+  getName(): string;
+}
+
+export interface PaymentOrderData {
+  items: Array<{
+    productId: string;
+    productName: string;
+    quantity: number;
+    price: number;
+    size?: string;
+    color?: string;
+  }>;
+  total: number;
+  currency?: string;
+  userId: number;
+}
+
+export interface PaymentResult {
+  success: boolean;
+  paymentId?: string;
+  orderId?: string;
+  data?: any;
+  error?: string;
+}
