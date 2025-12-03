b890fdc9f6eea27e6ce8b8be6268db1ef4c65b6f
alianjum-web <muhammadaliabbas7890@gmail.com>
2025-12-03 21:42:19 +0500

    clear the ocmmetns

--- client/src/components/common/layout.tsx
+++ client/src/components/common/layout.tsx
@@ -1,29 +1,3 @@
-// "use client";
-
-// import { usePathname } from "next/navigation";
-// import Header from "../user/header";
-
-
-// const pathsNotToShowHeaders = ["/auth", "/super-admin"];
-
-// function CommonLayout({ children }: { children: React.ReactNode }) {
-//   const pathName = usePathname();
-
-//   const showHeader = !pathsNotToShowHeaders.some((currentPath) =>
-//     pathName.startsWith(currentPath)
-//   );
-
-//   return (
-//     <div className="min-h-screen bg-white">
-      
-//       {showHeader && <Header />}
-//       <main>{children}</main>
-//     </div>
-//   );
-// }
-
-// export default CommonLayout;
-
 "use client";
 
 import { usePathname } from "next/navigation";
