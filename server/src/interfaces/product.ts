94c2aaaee0704c76ff97c55f2041952caf578a61
alianjum-web <muhammadaliabbas7890@gmail.com>
2025-12-06 15:25:42 +0500

    folder structure moved

--- server/src/interfaces/product.ts
+++
@@ -1,9 +0,0 @@
-export interface MinimalProduct {
-  productId: string;
-  productName: string;
-  productCategory: string;
-  quantity: number;     
-  size?: string | null;
-  color?: string | null;
-  price: number;
-}
