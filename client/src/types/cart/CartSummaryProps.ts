d3ae0eac06a3fb9125d838842386ddabbec74f5e
alianjum-web <muhammadaliabbas7890@gmail.com>
2026-01-03 08:05:39 +0500

    Removed unnessary props

--- client/src/types/cart/CartSummaryProps.ts
+++ client/src/types/cart/CartSummaryProps.ts
@@ -1,8 +1,5 @@
 export interface CartSummaryProps {
   subtotal: number;
-  shipping: number;
-  tax: number;
-  total: number;
   itemCount: number;
   onCheckout: () => void;
   onContinueShopping: () => void;
