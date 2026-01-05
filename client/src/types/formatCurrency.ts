3df517b582a11cb7a87b98552755226224e84bee
alianjum-web <muhammadaliabbas7890@gmail.com>
2026-01-05 19:55:22 +0500

    format quantity and currency

---
+++ client/src/types/formatCurrency.ts
@@ -0,0 +1,11 @@
+export const formatCurrency = (amount: number, currency = 'USD') => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 2,
+  }).format(amount);
+};
+
+export const formatQuantity = (quantity: number) => {
+  return quantity.toLocaleString('en-US');
+};
