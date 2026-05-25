cb47859add16b92be20836f6dcdcdc27650e2cf1
Ali Anjum <muhammadalikhanzada7890@gmail.com>
2026-05-25 20:38:49 +0500

    Added the wishlist schema for the zod

---
+++ server/src/validations/wishlistSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { productIdSchema } from "./cartSchema";
+
+export const toggleWishlistSchema = z.object({
+  productId: productIdSchema,
+});
+
+export const wishlistItemParamsSchema = z.object({
+  id: z.string().uuid("Invalid wishlist item id"),
+});
+
+export type ToggleWishlistInput = z.infer<typeof toggleWishlistSchema>;
