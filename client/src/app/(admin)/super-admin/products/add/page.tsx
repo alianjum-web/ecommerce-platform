50327ae8e52c6bb29cac00c03ddc5d814663445f
Ali Anjum <muhammadalikhanzada7890@gmail.com>
2026-05-31 17:13:49 +0500

    refactor(client): update imports in app/(admin)/super-admin/products/add/page.tsx

--- client/src/app/(admin)/super-admin/products/add/page.tsx
+++ client/src/app/(admin)/super-admin/products/add/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Suspense } from "react";
-import ProductForm from "@/components/super-admin/ProductForm";
+import ProductForm from "@/components/super-admin/organisms/ProductForm";
 import { 
   Package, 
   Zap, 
