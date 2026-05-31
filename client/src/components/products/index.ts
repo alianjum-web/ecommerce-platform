060b99d6ca1004d6dd88cb352eea557d5d6fcad4
Ali Anjum <muhammadalikhanzada7890@gmail.com>
2026-05-31 17:14:30 +0500

    refactor(client): update components/products/index.ts

---
+++ client/src/components/products/index.ts
@@ -0,0 +1,6 @@
+export { ProductGrid } from "./organisms/ProductGrid";
+export { ProductFilters } from "./molecules/ProductFilters";
+export { Pagination as ProductPagination } from "./molecules/ProductPagination";
+export { default as ProductManagementList } from "./organisms/ProductManagementList";
+export { useProductFilters } from "./hooks/useProductFilter";
+export { useProductCatalog } from "./hooks/useProductCatalog";
